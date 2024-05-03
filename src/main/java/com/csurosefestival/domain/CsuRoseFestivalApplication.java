838ef453229f85f1bc0e9e84544caba7c926d088
jongbinchoi <jongbin985@naver.com>
2024-05-03 18:36:34 +0900

    조선대학교 장미축제

---
+++ src/main/java/com/csurosefestival/domain/CsuRoseFestivalApplication.java
@@ -0,0 +1,13 @@
+package com.csurosefestival.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsuRoseFestivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsuRoseFestivalApplication.class, args);
+	}
+
+}
